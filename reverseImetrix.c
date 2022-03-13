529910ae168f4b4ddcdb06614b66c98860082ec8
Md Al Amin <alaminrakib4864@gmail.com>
2022-03-13 21:38:56 +0600

    see the demo https://prnt.sc/JLBnR7GVPCNW

---
+++ reverseImetrix.c
@@ -0,0 +1,27 @@
+/**
+ * @file reverseImetrix.c
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-03-13
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+void main(){
+    int row, col,n, i=1;
+    printf("Enter row number: ");
+    scanf("%d", &n);
+
+    for(row=n; row>=1; row--){
+        for(col=1; col<=n; col++){
+            if(row==col){
+                printf("%d ",i);
+            }else{
+                printf("* ");
+            }
+        }
+        printf("\n");
+    }
+}
